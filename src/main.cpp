b79fb329176e8f5bcd1d263e019e43eed1922eb9
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-24 23:42:38 +0200

    feat(App): Make use of CommandLine object in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
-#include "Nexpp/main.h"
+#include "Nexpp/CommandLine/CommandLine.h"
 
 #include <QApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
+  QApplication::setApplicationName("Nexpp");
+  QApplication::setApplicationVersion("0.0.1");
+
+  CommandLine command_line(app);
 
   return app.exec();
 }

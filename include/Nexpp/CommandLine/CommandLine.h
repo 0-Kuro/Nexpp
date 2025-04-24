096b7f0ff7d1b1bba5e7d1fff39a9a6bb5e41c06
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-25 01:31:59 +0200

    hotfix(Parser): Add critical missing components

--- include/Nexpp/CommandLine/CommandLine.h
+++ include/Nexpp/CommandLine/CommandLine.h
@@ -11,15 +11,18 @@ class CommandLine
 public:
   explicit CommandLine(const QApplication &application);
 
-  AppMode get_mode() const;
-  QString get_libraries() const;
-  QString get_standard() const;
-  bool    has_flags() const;
+  AppMode     get_mode() const;
+  QString     get_project_name() const;
+  QString     get_destination() const;
+  QStringList get_libraries() const;
+  Standard    get_standard() const;
+  bool        has_flags() const;
 
 private:
   void               setup_options();
 
   void               add_mode_option();
+  void               add_project_name_option();
   void               add_destination_option();
   void               add_libraries_option();
   void               add_standards_option();
@@ -30,9 +33,13 @@ private:
       const QString &value_name, const QStringList &allowed_values
   );
 
+  void               consume_options();
+
   QCommandLineParser m_parser;
 
   AppMode            m_mode;
+  QString            m_project_name;
+  QString            m_destination;
   QStringList        m_libraries;
   Standard           m_standard;
   bool               m_has_flags;
--- include/Nexpp/Types/Standard.h
+++ include/Nexpp/Types/Standard.h
@@ -25,3 +25,20 @@ inline const QString to_string(Standard standard) noexcept
     return "Invalid";
   }
 }
+
+inline Standard from_int(int standard) noexcept
+{
+  if(standard == 14) {
+    return Standard::CPP14;
+  }
+
+  if(standard == 17) {
+    return Standard::CPP17;
+  }
+
+  if(standard == 20) {
+    return Standard::CPP20;
+  }
+
+  return Standard::CPP23;
+}

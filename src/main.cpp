b8a5bb82aa11d03432e1eff78a898a4902016bc9
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-25 01:40:50 +0200

    chore(Nexpp): Make use of cmake base

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Nexpp/CommandLine/CommandLine.h"
+#include "Nexpp/Data/CMakeBase.h"
 
 #include <QApplication>
 #include <QCommandLineParser>
@@ -10,6 +11,21 @@ int main(int argc, char **argv)
   QApplication::setApplicationVersion("0.0.1");
 
   CommandLine command_line(app);
+
+  CMakeBase   cmake_base;
+
+  auto        result = cmake_base.setup_config(
+      command_line.get_project_name().toStdString(),
+      command_line.get_standard(), command_line.has_flags()
+  );
+
+  qDebug() << result;
+
+  if(command_line.get_mode() == AppMode::CLI) {
+    app.closeAllWindows();
+
+    return 0;
+  }
 
   return app.exec();
 }
